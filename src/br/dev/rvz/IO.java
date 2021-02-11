3b8d126c44b6fb0a24e30dbbd949e7227c9c9ec2
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-11 09:38:08 -0300

    criado modelo de entrada e saída de dados

---
+++ src/br/dev/rvz/IO.java
@@ -0,0 +1,16 @@
+package br.dev.rvz;
+
+import java.util.Scanner;
+
+/**
+ * entrada e saída de dados no terminal
+ * */
+public class IO {
+    public static Scanner input() {
+        return new Scanner(System.in);
+    }
+
+    public void output(String texto) {
+        System.out.println(texto);
+    }
+}
