21779dead0cec90dcce1ba678dc7d33eb03819b5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-11 09:35:32 -0300

    criado modelo de agenda

---
+++ src/br/dev/rvz/Agenda.java
@@ -0,0 +1,30 @@
+package br.dev.rvz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agenda {
+
+    private String nome;
+    private List<Contato> contatos;
+
+    public Agenda() {
+        this.contatos = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Contato> getContatos() {
+        return contatos;
+    }
+
+    public void setContatos(List<Contato> contatos) {
+        this.contatos = contatos;
+    }
+}
